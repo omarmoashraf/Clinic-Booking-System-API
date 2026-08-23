ff8b8f92b67cc2675970fea2a642060a8f7f3b5c
omarmoashraf <omarmoashraf05@gmail.com>
2026-08-23 23:33:33 +0300

    feat(specialty): repository with pagination and search

--- src/repositories/specialty.repository.js
+++ src/repositories/specialty.repository.js
@@ -3,3 +3,55 @@ import prisma from '../lib/prisma.js';
 export const findById = (id, client = prisma) => {
   return client.specialty.findUnique({ where: { id } });
 };
+
+export const findByName = (name , client = prisma) =>{
+  return client.specialty.findUnique({where: name});
+}
+
+export const findAll = async (
+  { page = 1, limit = 10, search },
+  client = prisma
+) => {
+  const skip = (page - 1) * limit;
+
+  const where = search
+    ? {
+        name: {
+          contains: search,
+          mode: 'insensitive',
+        },
+      }
+    : {};
+
+  const [specialties, total] = await Promise.all([
+    client.specialty.findMany({
+      where,
+      skip,
+      take: limit,
+      orderBy: { name: 'asc' },
+    }),
+    client.specialty.count({
+      where,
+    }),
+  ]);
+
+  return {
+    specialties,
+    total,
+  };
+};
+
+
+export const createSpecialty = (data , client = prisma) =>{
+  return client.specialty.create({data});
+};
+
+export const updateSpecialty = (id , data , client = prisma)=>{
+
+  return client.specialty.update({where: {id}, data});
+}
+
+
+export const deleteSpecialty = (id , client = prisma) =>{
+  return client.specialty.delete({where: {id}});
+}
